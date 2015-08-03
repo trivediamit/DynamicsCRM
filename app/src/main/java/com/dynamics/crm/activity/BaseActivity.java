c026bbc382d4361c55e4af541209e80c1277ec34
Amit Trivedi <amitt@winjit.com>
2015-08-03 23:54:32 +0530

    Comments added

--- app/src/main/java/com/dynamics/crm/activity/BaseActivity.java
+++ app/src/main/java/com/dynamics/crm/activity/BaseActivity.java
@@ -20,6 +20,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        // TODO to be used in separate activities
         setupToolbar();
     }
 
